ab809544022197497110e2fe7f449895027765ff
ruslan.k <ruslan.k@1fit.app>
2022-11-23 22:15:58 +0600

    mediaplayer added

--- src/AnimePage/AnimePage.jsx
+++ src/AnimePage/AnimePage.jsx
@@ -9,30 +9,66 @@ import {
   Image,
   Row,
   Spinner,
+  Modal,
 } from "react-bootstrap";
+import ReactPlayer from "react-player";
+import { Player } from "video-react";
+import Iframe from "react-iframe";
 
 import "./AnimePage.css";
 
 function AnimePage() {
   const [animeDetail, setAnimeDetail] = useState({});
+  const [selectedEpisode, setSelectedEpisode] = useState({});
+  const [episodeUrl, setEpisodeUrl] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
   const { name } = useParams();
+
+  const handleShow = (episode) => {
+    setIsOpen(true);
+    setSelectedEpisode(episode);
+  };
 
   useEffect(() => {
     axios
       .get(`https://gogoanime.consumet.org/anime-details/${name}`)
       .then((response) => setAnimeDetail(response.data));
-  }, [name]);
+    axios
+      .get(`https://gogoanime.consumet.org/vidcdn/watch/${selectedEpisode}`)
+      .then((response) => setEpisodeUrl(response.data));
+  }, [name, selectedEpisode]);
+
+  console.log(episodeUrl);
 
   const episodeList = animeDetail.episodesList?.map((episode) => {
     return (
-      <a key={episode.episodeId} href={episode.episodeUrl} target="_blank" rel="noopener noreferrer">
+      <button
+        key={episode.episodeId}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={() => handleShow(episode.episodeId)}
+      >
         Episode: <b>{episode.episodeNum}</b>
-      </a>
+      </button>
     );
   });
 
   return (
     <div className="AnimePage">
+      <Modal
+        style={{ maxWidth: "100%" }}
+        show={isOpen}
+        hide={() => setIsOpen(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Episode: {selectedEpisode.episodeNum}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ width: "100vw", height: "100vh" }}>
+          {episodeUrl.sources && (
+            <Iframe height="100%" width="100%" url={episodeUrl.Referer} />
+          )}
+        </Modal.Body>
+      </Modal>
       {animeDetail.episodesList ? (
         <Container fluid>
           <div className="main">
